9e4b442dc60f915540410b7d8155409437ccb8cb
matvejcekasov <chekashovmatvey@gmail.com>
2024-02-27 15:33:23 +0300

    feat: Реализована функция для переделки большинства классов в дружественные

--- mixins/friend_class.py
+++ mixins/friend_class.py
@@ -24,3 +24,31 @@ class FriendMixin:
             name = name.replace(f'_{caller.__class__.__name__}', '')
             return getattr(self, f'_{self.__class__.__name__}{name}')
         return self.__getattribute__(name)
+
+
+def auto_friend():
+    """
+    Если необходимо сделать все классы в Python проекте дружественными,
+    запустите эту функцию.
+
+    Меняет __getattr__ для всех имеющихся классов (кроме тех, у которых нельзя
+    изменить __getattr__)ю
+    """
+    import inspect
+
+    def __getattr__(self, name):
+        stack = inspect.stack()
+        frame = stack[1][0]
+        caller = frame.f_locals.get('self', None)
+        if caller.__class__ in self.__friends:
+            name = name.replace(f'_{caller.__class__.__name__}', '')
+            return getattr(self, f'_{self.__class__.__name__}{name}')
+        return self.__getattribute__(name)
+
+    for i in object.__subclasses__():
+        if not getattr(i, '__getattr__', None):
+            try:
+                i.__getattr__ = __getattr__
+                print(i)
+            except TypeError:
+                pass
